6df7238e941770b86678bfe9b607266643b860ec
Tho Tran <thotran9@mit.edu>
2017-11-25 21:33:52 -0500

    Changed the names of the variables to more intuitive names; Added comments

--- flask-intro/app.py
+++ flask-intro/app.py
@@ -9,49 +9,23 @@ app.secret_key='hi'
 app.database='database.db'
 
 #login required decorator/ login page is always popped up first
-def login_required(f):
-	@wraps(f)
-	def wrap(*args, **kwargs):
-		if 'logged_in' in session:
-			return f(*args, **kwargs)
-		else:
-			flash('You need to login first')
-			return redirect(url_for('login'))
-	return wrap
-
-
-
-# use decorators to link the function to a url
-@app.route('/')
-# call the func login_required to make sure people are loggined in to enter home page.
-#@login_required
-def home():
-	#create a db connection obj right after logged in
-	g.db=connect_db()
-	#query the database/fetching data from the database
-	cur=g.db.execute('select * from submissions')
-	#store the fetched data in dictionaries, each post is in form of a dictionary
-	submissions=[dict(sub_num= i, happy=row[0], excited=row[1]) for i, row in enumerate(cur.fetchall())]
-	#close database
-	g.db.close()
-	return render_template('vizpage.html', submissions=submissions)
-
-@app.route('/welcome')
-def welcome():
-    return render_template('welcome.html')  # render a template
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-##        error = None
-##        level = None
-##        if request.method == 'POST':
-##                level = request.form['level']
-##                
-##                return redirect(url_for('mood_map'))
+##def login_required(f):
+##	@wraps(f)
+##	def wrap(*args, **kwargs):
+##		if 'logged_in' in session:
+##			return f(*args, **kwargs)
+##		else:
+##			flash('You need to login first')
+##			return redirect(url_for('login'))
+##	return wrap
 ##
-##        else:
-##                error = 'no emotions. Please try again'
-        return render_template('login.html')
+##@app.route('/welcome')
+##def welcome():
+##    return render_template('welcome.html')  # render a template
+
+@app.route('/', methods=['GET', 'POST'])
+def input():
+        return render_template('input.html')
 
 @app.route('/add_submission', methods = ['GET', 'POST'])
 def add_submission():
@@ -68,23 +42,38 @@ def add_submission():
                 msg='Submission recorded successfully'
         except:
             conn.rollback()
-            msg = 'why error in insert operation'
+            msg = 'error in insert operation'
         finally:
             return render_template('result.html', msg = msg)
             conn.close()
 
-
-
+# use decorators to link the function to a url
 @app.route('/mood_map')
-def mood_map():
-    return render_template('vizpage.html')  # render a template
+# call the func login_required to make sure people are loggined in to enter home page.
+#@login_required
+def query_submissions():
+	#create a db connection obj right after user input data
+	g.db=connect_db()
+	#query the database/fetching data from the database
+	cur=g.db.execute('select * from submissions')
+	#store the fetched data in dictionaries, each submission is in form of a dictionary
+	submissions=[dict(sub_num= i, happy=row[0], excited=row[1]) for i, row in enumerate(cur.fetchall())]
+	#close database
+	g.db.close()
+	return render_template('vizpage.html', submissions=submissions)
 
-@app.route('/logout')
-@login_required
-def logout():
-	session.pop('logged_in', None)
-	flash('You are logged out')
-	return redirect(url_for('welcome'))
+##
+##
+##@app.route('/mood_map')
+##def mood_map():
+##    return render_template('vizpage.html')  # render a template
+
+##@app.route('/logout')
+##@login_required
+##def logout():
+##	session.pop('logged_in', None)
+##	flash('You are logged out')
+##	return redirect(url_for('welcome'))
 
 #connect the app to the database
 def connect_db():
